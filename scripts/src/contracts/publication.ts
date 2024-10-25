1e551b21de6dc16b5ab53e0245cb47c83f5ac935
Ikem Peter <ikempeter2020@gmail.com>
2024-10-25 11:56:39 +0100

    publication script

--- scripts/src/contracts/publication.ts
+++ scripts/src/contracts/publication.ts
@@ -1,6 +1,10 @@
 import tokenbound from "../index";
-import {KARST_PUBLICATION_CONTRACT_ADDRESS, PROFILE_ADDRESS_ONE, PROFILE_ADDRESS_TWO} from "../helpers/constants"
 import { Call } from "starknet-tokenbound-sdk";
+import {
+    KARST_PUBLICATION_CONTRACT_ADDRESS,
+    PROFILE_ADDRESS_ONE,
+    PROFILE_ADDRESS_TWO,
+} from "../helpers/constants";
 
 //collect
 //upvote
@@ -9,84 +13,94 @@ import { Call } from "starknet-tokenbound-sdk";
 //comment
 //post
 
-const execute_post = async() =>{
-    let call:Call = {
-        to:KARST_PUBLICATION_CONTRACT_ADDRESS,
-        selector:"0x70cb483ff70f4401877815e8a7bada80205df630fe0d8157b4e458f4e5114e",
-        calldata:[{content_URI: "test post publication...", profile_address: PROFILE_ADDRESS_TWO, channel_id: "0x1"}]
-        }
-        try {
-            const Resp = await tokenbound?.execute(PROFILE_ADDRESS_TWO, [call])
-            console.log('execution-response=:', Resp)
-        } catch (error) {
-            console.log(error)
-        }
-    
-}
+const execute_post = async () => {
+    let call: Call = {
+        to: KARST_PUBLICATION_CONTRACT_ADDRESS,
+        selector:
+            "0x3023f17c6c151428a83e388ec4de34e239b102d8cb4b01068f4cdc2ed6b83b6",
+        calldata: [{
+            content_URI: "test post publication...",
+            profile_address: PROFILE_ADDRESS_TWO,
+            channel_id: "0x1",
+        }],
+    };
+    try {
+        const Resp = await tokenbound?.execute(PROFILE_ADDRESS_TWO, [call]);
+        console.log("execution-response=:", Resp);
+    } catch (error) {
+        console.log(error);
+    }
+};
 
-const execute_comment = async() =>{
-    let call:Call = {
-        to:KARST_PUBLICATION_CONTRACT_ADDRESS,
-        selector:"0x70cb483ff70f4401877815e8a7bada80205df630fe0d8157b4e458f4e5114e",
-        calldata:[{
-            content_URI: "test comment publication...", 
-            profile_address: PROFILE_ADDRESS_TWO, 
-            pointed_profile_address: PROFILE_ADDRESS_ONE, 
-            pointed_pub_id: "0x1", 
-            reference_pub_type: "0x1"
-        }]
-        }
-        try {
-            const Resp = await tokenbound?.execute(PROFILE_ADDRESS_TWO, [call])
-            console.log('execution-response=:', Resp)
-        } catch (error) {
-            console.log(error)
-        }
-    
-}
+const execute_comment = async () => {
+    let call: Call = {
+        to: KARST_PUBLICATION_CONTRACT_ADDRESS,
+        selector:
+            "0x29ce70c72e2e7191b71ef82881773fbf30510f24a3450f02d555b5f04ac9702",
+        calldata: [{
+            content_URI: "test comment publication...",
+            profile_address: PROFILE_ADDRESS_TWO,
+            pointed_profile_address: PROFILE_ADDRESS_ONE,
+            pointed_pub_id: "0x1",
+            reference_pub_type: "0x1",
+        }],
+    };
+    try {
+        const Resp = await tokenbound?.execute(PROFILE_ADDRESS_TWO, [call]);
+        console.log("execution-response=:", Resp);
+    } catch (error) {
+        console.log(error);
+    }
+};
 
-const execute_repost = async() =>{
-    let call:Call = {
-        to:KARST_PUBLICATION_CONTRACT_ADDRESS,
-        selector:"0x70cb483ff70f4401877815e8a7bada80205df630fe0d8157b4e458f4e5114e",
-        calldata:[{
-            profile_address: PROFILE_ADDRESS_TWO, 
-            pointed_profile_address: PROFILE_ADDRESS_ONE, 
-            pointed_pub_id: "0x1", 
-        }]
-        }
-        try {
-            const Resp = await tokenbound?.execute(PROFILE_ADDRESS_TWO, [call])
-            console.log('execution-response=:', Resp)
-        } catch (error) {
-            console.log(error)
-        }
-}
+const execute_repost = async () => {
+    let call: Call = {
+        to: KARST_PUBLICATION_CONTRACT_ADDRESS,
+        selector:
+            "0x1d2dbb7f2c4890417100d064e363f8dc67af1dbdf7fd6cc9538d82c7566c39b",
+        calldata: [{
+            profile_address: PROFILE_ADDRESS_TWO,
+            pointed_profile_address: PROFILE_ADDRESS_ONE,
+            pointed_pub_id: "0x1",
+        }],
+    };
+    try {
+        const Resp = await tokenbound?.execute(PROFILE_ADDRESS_TWO, [call]);
+        console.log("execution-response=:", Resp);
+    } catch (error) {
+        console.log(error);
+    }
+};
 
-const execute_upvote = async() =>{
-    let call:Call = {
-        to:KARST_PUBLICATION_CONTRACT_ADDRESS,
-        selector:"0x70cb483ff70f4401877815e8a7bada80205df630fe0d8157b4e458f4e5114e",
-        calldata:[PROFILE_ADDRESS_TWO, "0x1"]
-        }
-        try {
-            const Resp = await tokenbound?.execute(PROFILE_ADDRESS_TWO, [call])
-            console.log('execution-response=:', Resp)
-        } catch (error) {
-            console.log(error)
-        }
-}
 
-const execute_downvote = async() =>{
-    let call:Call = {
-        to:KARST_PUBLICATION_CONTRACT_ADDRESS,
-        selector:"0x70cb483ff70f4401877815e8a7bada80205df630fe0d8157b4e458f4e5114e",
-        calldata:[PROFILE_ADDRESS_TWO, "0x1"]
-        }
-        try {
-            const Resp = await tokenbound?.execute(PROFILE_ADDRESS_TWO, [call])
-            console.log('execution-response=:', Resp)
-        } catch (error) {
-            console.log(error)
-        }
-}
+const execute_upvote = async () => {
+    let call: Call = {
+        to: KARST_PUBLICATION_CONTRACT_ADDRESS,
+        selector:
+            "0x2051ad9768cb00721e0522429c7257f589ea08adf6eb43865204cf1cc60b61c",
+        calldata: [PROFILE_ADDRESS_TWO, "0x1"],
+    };
+    try {
+        const Resp = await tokenbound?.execute(PROFILE_ADDRESS_TWO, [call]);
+        console.log("execution-response=:", Resp);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const execute_downvote = async () => {
+    let call: Call = {
+        to: KARST_PUBLICATION_CONTRACT_ADDRESS,
+        selector:
+            "0x4cbaff62234695102a38001164ecf4c8534f4771b1eabdf5c19fd8384157a5",
+        calldata: [PROFILE_ADDRESS_TWO, "0x1"],
+    };
+    try {
+        const Resp = await tokenbound?.execute(PROFILE_ADDRESS_TWO, [call]);
+        console.log("execution-response=:", Resp);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+execute_post();
